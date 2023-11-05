c0858fd5a696617c294aae08f11e64639f1eb114
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-11-05 20:22:27 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace zadatak12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mCal1 = new System.Windows.Forms.MonthCalendar();
+            this.but1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mCal1
+            // 
+            this.mCal1.Location = new System.Drawing.Point(267, 18);
+            this.mCal1.Name = "mCal1";
+            this.mCal1.TabIndex = 0;
+            // 
+            // but1
+            // 
+            this.but1.Location = new System.Drawing.Point(327, 214);
+            this.but1.Name = "but1";
+            this.but1.Size = new System.Drawing.Size(75, 23);
+            this.but1.TabIndex = 1;
+            this.but1.Text = "KLIKNI";
+            this.but1.UseVisualStyleBackColor = true;
+            this.but1.Click += new System.EventHandler(this.but1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.but1);
+            this.Controls.Add(this.mCal1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar mCal1;
+        private System.Windows.Forms.Button but1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadatak12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void but1_Click_1(object sender, EventArgs e)
+        {
+            DateTime start = mCal1.SelectionStart;
+            DateTime end = mCal1.SelectionEnd;
+
+            if (start == end)
+            {
+                MessageBox.Show("Molimo vas da odaberete opseg datuma.");
+                return;
+            }
+
+            int days = (int)(end - start).TotalDays + 1; // dodajemo 1 da bismo uključili i poslednji dan
+            MessageBox.Show($"Broj odabranih dana: {days}");
+
+            string startDateString = start.ToShortDateString();
+            string endDateString = end.ToShortDateString();
+            MessageBox.Show($"Izabrani opseg datuma: od {startDateString} do {endDateString}");
+
+        }
+    }
+}
